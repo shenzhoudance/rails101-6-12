9b15d516b8a07fa44818f230f08dce0d695c94bb
xiaowei <shenzhoudance@gmail.com>
2017-06-12 06:32:56 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
 
    def index
      @groups = Group.all
@@ -29,20 +29,50 @@ class GroupsController < ApplicationController
      end
 
      def edit
+       @group = Group.find(params[:id])
       end
 
-      def update
-        if @group.update(group_params)
+        def update
+          @group = Group.find(params[:id])
+
+          @group.update(group_params)
+
           redirect_to groups_path, notice: "Update Success"
-        else
-          render :edit
         end
-      end
 
-      def destroy
-        @group.destroy
-        redirect_to groups_path, alert: "Group deleted"
-      end
+        def destroy
+           @group = Group.find(params[:id])
+           @group.destroy
+           flash[:alert] = "Group deleted"
+           redirect_to groups_path
+         end
+
+      def join
+        @group = Group.find(params[:id])
+
+         if !current_user.is_member_of?(@group)
+           current_user.join!(@group)
+           flash[:notice] = "加入本讨论版成功！"
+         else
+           flash[:warning] = "你已经是本讨论版成员了！"
+         end
+
+         redirect_to group_path(@group)
+       end
+
+       def quit
+         @group = Group.find(params[:id])
+
+         if current_user.is_member_of?(@group)
+           current_user.quit!(@group)
+           flash[:alert] = "已退出本讨论版！"
+         else
+           flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+         end
+
+         redirect_to group_path(@group)
+       end
+
 
   private
 
@@ -53,6 +83,7 @@ class GroupsController < ApplicationController
       redirect_to root_path, alert: "You have no permission."
     end
   end
+
 
   def group_params
     params.require(:group).permit(:title, :description)
