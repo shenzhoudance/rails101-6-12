2bd791a09197effbfbbead11919d429cdc35b5b8
xiaowei <shenzhoudance@gmail.com>
2017-06-12 06:54:21 +0800

    implement group relation

--- db/migrate/20170611140729_create_group_relationships.rb
+++ db/migrate/20170611140729_create_group_relationships.rb
@@ -1,5 +1,8 @@
 class CreateGroupRelationships < ActiveRecord::Migration[5.0]
-  def change
+  belongs_to :group
+  belongs_to :user
+
+def change
     create_table :group_relationships do |t|
       t.integer :group_id
       t.integer :user_id
